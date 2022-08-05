77494763ebe9b2b9b06c754e4da9d9b10bcecaf9
jitsatya <satyajitnamasudra@outlook.com>
2022-08-05 09:56:07 +0530

    Create variables.py

---
+++ Basics/variables.py
@@ -0,0 +1,22 @@
+# Python is dynamically Typed
+name = "Satya"
+print(name)
+# No variable declaration
+name = 4
+print(name)
+name = True
+print(name)
+# Dynamic Binding: A variable can store any type of data
+# Special Declaration syntax
+a=5;b=6;c=7 # initializing variables in single line
+print(a)
+print(b)
+print(c)
+
+# Can also be done this way
+a,b,c = 4,5,6
+print(a,b,c)
+
+# Initializing 3 variables with same value
+a=b=c=7
+print(a,b,c)
