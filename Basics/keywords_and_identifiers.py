cae74e5f581bb78d40eff75f528c3e6e814a91e6
jitsatya <satyajitnamasudra@outlook.com>
2022-08-05 10:06:40 +0530

    Create keywords_and_identifiers.py

---
+++ Basics/keywords_and_identifiers.py
@@ -0,0 +1,13 @@
+# Keywords and identifiers in python
+# python is a case sensitive programming language
+# Keyword is a reserved word in any programming language
+import keyword
+print (keyword.kwlist) # List of keywords
+
+# Identifiers: Used to Identify a variable, function, class, module, or other object
+## Can only start with an alphabet
+## Followed by 0 or more letters, _ and digits
+## Keywords cannot be used as an identifiers
+
+first_name = "Satyajit"
+print(first_name)
